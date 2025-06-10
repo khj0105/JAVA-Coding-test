80a5f9f0481d04600cb93bc50fcf6c81a4115a09
khj0105 <khj0001052@gmail.com>
2025-06-10 23:41:06 +0900

    20250610 코딩 연습

--- ct/src/ct/week04/programmers/practice03.java
+++ ct/src/ct/week04/programmers/practice03.java
@@ -1,5 +1,24 @@
-package ct.week04.programmers;
+package ct.week04;
 
-public class practice03 {
+import java.util.Scanner;
+import java.util.Stack;
 
+public class BJ28278 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+		Stack<Integer> stack = new Stack<>();
+		int x = sc.nextInt();
+		for(int i = 0; i < x; i++) {
+			String s = sc.nextLine();
+			String cmd = s.split(" ") [0];
+		switch(cmd) {
+		case "1" :
+			stack.push(Integer.parseInt(s.split(" ")[1]));
+			break;
+		case "2" :
+			stack.
+		
+		}
+	}
 }
